ad8dc90347b5f955f29162c826670deeb652bee1
Ram <ramlevy@gmail.com>
2018-06-20 21:31:48 +0300

    Styling for mobile

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption} // check if selectedOption has a value
         contentLabel="Selected Option"
         onRequestClose = {props.handleClearSelectedOption}
+        closeTimeoutMS = {200}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearSelectedOption}>Okay </button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleClearSelectedOption}>Okay </button>
     </Modal>
 
 );
